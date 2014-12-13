0b9f57bca0e630e4d3adcd11aecd85bd91e7777b
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2014-12-13 20:14:18 +0100

    Getting access token

--- mobile/src/main/java/com/jpettersson/wearify/MainActivity.java
+++ mobile/src/main/java/com/jpettersson/wearify/MainActivity.java
@@ -1,19 +1,50 @@
 package com.jpettersson.wearify;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.authentication.SpotifyAuthentication;
 
 
 public class MainActivity extends Activity {
 
+    private static final String TAG = "MainActivity";
+    private static final String CLIENT_ID = "460d2cafee9d4cf39f913b131bcc80b5";
+    private static final String REDIRECT_URI = "com-jpettersson-wearify://callback";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "Awake!");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    public void authenticate(View view) {
+        SpotifyAuthentication.openAuthWindow(CLIENT_ID, "token", REDIRECT_URI,
+                new String[]{"playlist-read-private"}, null, this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.i(TAG, "New intent!");
+        Uri uri = intent.getData();
+        if (uri != null) {
+            AuthenticationResponse response = SpotifyAuthentication.parseOauthResponse(uri);
+            String accessToken = response.getAccessToken();
+            Log.i(TAG, "Authentication successful!");
+            Log.i(TAG, "accessToken:" + accessToken);
+        }else{
+            Log.i(TAG, "Authentication failed!");
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
