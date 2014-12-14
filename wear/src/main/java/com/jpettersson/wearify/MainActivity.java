b1ead0f1a19a70e2d8a03b3960952b06507664ac
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2014-12-14 22:46:36 +0100

    Render list on watch

--- wear/src/main/java/com/jpettersson/wearify/MainActivity.java
+++ wear/src/main/java/com/jpettersson/wearify/MainActivity.java
@@ -4,6 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.google.android.gms.wearable.DataMap;
@@ -13,11 +18,13 @@ import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends Activity {
 
     private static final String TAG = "MainActivity";
-    private TextView mTextView;
+    private ListView mPlaylistListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +34,48 @@ public class MainActivity extends Activity {
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+                mPlaylistListView = (ListView) stub.findViewById(R.id.playlist_list);
+
+                byte[] playlistByteArray = readPlaylistFile();
+                if(playlistByteArray != null) {
+                    DataMap dataMap = DataMap.fromByteArray(playlistByteArray);
+                    Log.i(TAG, dataMap.toString());
+
+                    ArrayList<DataMap> playlistItems = dataMap.getDataMapArrayList("playlistItems");
+                    renderList(playlistItems);
+                }else{
+                    Log.i(TAG, "Playlist file was null!");
+                }
             }
         });
 
-        byte[] playlistByteArray = readPlaylistFile();
-        if(playlistByteArray != null) {
-            DataMap dataMap = DataMap.fromByteArray(playlistByteArray);
+    }
 
-            Log.i(TAG, dataMap.toString());
-        }else{
-            Log.i(TAG, "Playlist file was null!");
+    private void renderList(ArrayList<DataMap> playlistDataMaps) {
+        final ArrayList<HashMap<String, String>> playlistItemList = new ArrayList<HashMap<String, String>>();
+
+        for(DataMap dataMap : playlistDataMaps) {
+            HashMap<String, String> map = new HashMap<String, String>();
+            map.put("uri",  dataMap.getString("uri"));
+            map.put("name", dataMap.getString("name"));
+            playlistItemList.add(map);
         }
+
+        ListAdapter adapter = new SimpleAdapter(this, playlistItemList , R.layout.playlist_item,
+                new String[] { "name" },
+                new int[] { R.id.name_text });
+
+
+//        final ListView lv = (ListView)findViewById(R.id.playlist_list);
+        mPlaylistListView.setAdapter(adapter);
+
+        mPlaylistListView.setTextFilterEnabled(true);
+        mPlaylistListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String playlistUri = playlistItemList.get(position).get("uri");
+                Log.i(TAG, "Open playlist: " + playlistUri);
+            }
+        });
     }
 
     private byte[] readPlaylistFile() {
